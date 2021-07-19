a62f711315835df61bc10c7a3473a232c0dd3d9c
Embo <79663490+electroma97@users.noreply.github.com>
2021-07-19 21:16:42 +0200

    Added GLFW and Glad dependencies with their premake build script

--- premake5.lua
+++ premake5.lua
@@ -4,12 +4,26 @@ workspace "ToyGL"
 	startproject "ToyGLEngine"
 	
 	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}" -- ex: Debug-Windows-x64
+
+include "projects/ToyEngine/thirdparty/Glad"
+include "projects/ToyEngine/thirdparty/GLFW"
 	
 project "ToyEngine"
-	location "projects/%{prj.name}"
+	location "projects/ToyEngine"
 	kind "ConsoleApp"
 	language "C++"
 	cppdialect "C++17"
 	
 	targetdir ("build/bin/" .. outputdir .. "/%{prj.name}")
 	objdir    ("build/obj/" .. outputdir .. "/%{prj.name}")
+
+	filter "system:windows"
+        systemversion "latest"
+
+	filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"

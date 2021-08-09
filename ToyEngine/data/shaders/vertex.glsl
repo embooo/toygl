2c82b2ea486a0c62d369096aac224303aa7031f6
Embo <79663490+electroma97@users.noreply.github.com>
2021-08-09 20:31:16 +0200

    Added Model, View, Projection matrices to vertex shader

--- ToyEngine/data/shaders/vertex.glsl
+++ ToyEngine/data/shaders/vertex.glsl
@@ -3,10 +3,14 @@
 layout(location = 0) in vec3 aPos;
 layout(location = 3) in vec3 aColor;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 out vec3 color;
 
 void main()
 {
     color = aColor;
-    gl_Position = vec4(aPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 1.0f);
 }

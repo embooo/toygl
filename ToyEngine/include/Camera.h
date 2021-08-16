d7681b7478e4d18cd5a3c7f505183786f0d427ce
Embo <79663490+electroma97@users.noreply.github.com>
2021-08-16 18:10:08 +0200

    Implemented camera movement and look around

--- ToyEngine/include/Camera.h
+++ ToyEngine/include/Camera.h
@@ -2,12 +2,11 @@
 
 #include <string>
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 #include "Event.h"
 #include "KeyboardEvent.h"
 #include "MouseEvent.h"
 #include "IObserver.h"
+#include "Window.h"
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
@@ -18,10 +17,9 @@
 struct ViewFrustum
 {
     ViewFrustum()
-        : fieldOfView(glm::radians(45.0f)), aspectRatio(1.0f), zNear(0.1f), zFar(100.0f) {}; 
-
-    ViewFrustum(float fov, float aspectRatio, float zNear = 0.1f, float zFar = 100.0f)
-        : fieldOfView(fov), aspectRatio(aspectRatio), zNear(zNear), zFar(zFar) {};
+        : fieldOfView(45.0f), windowWidth(1280), windowHeight(720), aspectRatio(16.0f/9.0f), zNear(0.1f), zFar(100.0f) {};
+    ViewFrustum(float fov, float width, float height, float zNear = 0.1f, float zFar = 100.0f)
+        : fieldOfView(fov), windowWidth(width), windowHeight(height), aspectRatio(width/height), zNear(zNear), zFar(zFar) {};
 
     // Data to build a projection matrix 
     // Angle (in radians) between the side or top/bottom planes of the frustum
@@ -32,6 +30,8 @@ struct ViewFrustum
     float zNear; 
     // Far depth limit - Distance in the Z-axis between the camera and far plane
     float zFar;  
+
+    float windowWidth, windowHeight;
 };
 
 namespace Axis
@@ -41,14 +41,18 @@ namespace Axis
     static const glm::vec3 Z = glm::vec3(0.0f, 0.0f, 1.0f);
 };
 
-
+namespace Mouse
+{
+    static glm::vec2 LastPos = glm::vec2(0.0f, 0.0f);
+    static glm::vec2 LastClickPos = glm::vec2(0.0f, 0.0f);
+    static glm::vec2 Delta = glm::vec2(0.0f, 0.0f);;
+}
 
 class Camera : public IObserver
 {
 public:
     Camera();
-    Camera(const std::string& name,   
-    const ViewFrustum& frustum,
+    Camera(const std::string& name, Window* window, const ViewFrustum& frustum,
     const glm::vec3& pos = glm::vec3(0.0f, 0.0f, 0.0f),
     const glm::vec3& target = glm::vec3(0.0f, 0.0f, -1.0f),
     const glm::vec3& up  = glm::vec3(0.0f, 1.0f, 0.0f) );
@@ -57,9 +61,12 @@ public:
     const glm::mat4& getProjectionMat() const;
 
     void move(const glm::vec3& dir, float deltaTime);
+    void rotate(float deltaTime);
 
     virtual void onUpdate(Event& event) override;
     void update(const float dt = 0.1f) ;
+    
+    
 
     // Mouse/Keyboard events handler
     virtual void onKeyPressed(KeyEvent& event);
@@ -67,10 +74,9 @@ public:
     virtual void onMouseClickEvent(MouseClick& event);
     virtual void onMouseMove(MouseMove& event);
     virtual void onMouseScroll(MouseScroll& event);
-
     
 private:
-
+    Window* m_WindowHandle;
     std::string name;
 
     // Viewing volume - everything inside is visible
@@ -83,13 +89,27 @@ private:
     // Default position of the camera - apex of the view frustum
     glm::vec3 position;
     // LookAt direction
-    glm::vec3 target;
+    glm::vec3 front; // aka target
     // Normalized vector - camera's up axis
     glm::vec3 up;
 
-    float m_BaseSpeed;
+    glm::vec3 right;
 
-    bool bLookAround;
-    bool bMoveUp, bMoveDown, bMoveLeft, bMoveRight;
+    void updateViewMatrix();
+
+    float m_MoveSpeed;
+    float m_LookSpeed;
+
+    float rotYawAmount = 0.0f, rotPitchAmount = 0.0f;
+    float horizontalSensitivity = 0.1f, verticalSensitivity = 0.1f;
+
+    float m_deltaTime;
+    float pitch = 0.0f;
+    float yaw = -90.0f;
+    bool bOutdatedViewMatrix = true;
+    bool firstClick = true;
+    bool bMouseMoved = false;
+    bool bMoveUp = false, bMoveDown = false, bMoveFront =false, bMoveBack = false, bMoveLeft = false, bMoveRight = false;
+    bool lookAroundState = false;
 
 };
--- ToyEngine/src/Camera.cpp
+++ ToyEngine/src/Camera.cpp
@@ -1,19 +1,23 @@
 #include "Camera.h"
 
 Camera::Camera()
-    : name("DefaultCamera"), viewFrustum(ViewFrustum()), position(glm::vec3(0.0f,0.0f,0.0f)), target(glm::vec3(0.0f, 0.0f, -1.0f)), up(glm::vec3(0.0f, 1.0f, 0.0f)), 
-    view(glm::lookAt(position, position + target, up)),
-    projection(glm::perspective(viewFrustum.fieldOfView, viewFrustum.aspectRatio, viewFrustum.zNear, viewFrustum.zFar)), m_BaseSpeed(0.5f), bLookAround(false)
+    : name("DefaultCamera"), 
+    viewFrustum(ViewFrustum()), 
+    position(glm::vec3(0.0f,0.0f, 5.0f)), front(glm::vec3(0.0f, 0.0f, -1.0f)), up(glm::vec3(0.0f, 1.0f, 0.0f)), 
+    view(glm::lookAt(position, position + front, up)),
+    projection(glm::perspective(viewFrustum.fieldOfView, viewFrustum.aspectRatio, viewFrustum.zNear, viewFrustum.zFar)), 
+    m_MoveSpeed(1.0f), m_LookSpeed (0.25f)
 {
-    bMoveUp= bMoveDown= bMoveLeft= bMoveRight = false;
+    right = glm::normalize(glm::cross(front, up));
 }
 
-Camera::Camera(const std::string& name, const ViewFrustum& frustum, const glm::vec3& pos, const glm::vec3& target, const glm::vec3& up)
-    : name(name), position(pos), target(target), up(up), viewFrustum(frustum), 
-    view(glm::lookAt(pos, pos + target, up)),
-    projection(glm::perspective(frustum.fieldOfView, frustum.aspectRatio, frustum.zNear, frustum.zFar)), m_BaseSpeed(0.5f), bLookAround(false)
+Camera::Camera(const std::string& name, Window* window, const ViewFrustum& frustum, const glm::vec3& pos, const glm::vec3& front, const glm::vec3& up)
+    : name(name), position(pos), front(glm::normalize(front)), up(glm::normalize(up)), viewFrustum(frustum), 
+    view(glm::lookAt(pos, glm::normalize(pos + front), up)),
+    projection(glm::perspective(frustum.fieldOfView, frustum.aspectRatio, frustum.zNear, frustum.zFar)), m_MoveSpeed(1.0f),m_LookSpeed (0.25f), m_WindowHandle(window)
 {
-    bMoveUp= bMoveDown= bMoveLeft= bMoveRight = false;
+    right = glm::cross(front, up);
+
 }
 
 const glm::mat4& Camera::getViewMat() const
@@ -50,73 +54,135 @@ void Camera::onUpdate(Event& event)
         case EventType::MouseScroll:
             onMouseScroll(static_cast<MouseScroll&>(event));
             break;
-
     }
 }
 
 void Camera::update(float deltaTime)
 {
-    if (bMoveUp)    move(-Axis::Z, deltaTime);
-    if (bMoveDown)  move(Axis::Z, deltaTime);
+    m_deltaTime = deltaTime;
 
-    if (bMoveRight) move(Axis::X, deltaTime);
-    if (bMoveLeft)  move(-Axis::X, deltaTime);
+    if (bMoveUp)       move(up,  deltaTime);
+    if (bMoveDown)     move(-up,  deltaTime);
+
+    if (bMoveFront)    move(front,  deltaTime);
+    if (bMoveBack)     move(-front, deltaTime);
+
+    if (bMoveRight) move(right, deltaTime);
+    if (bMoveLeft)  move(-right, deltaTime);
+
+    
+    if(bOutdatedViewMatrix)
+    {
+        updateViewMatrix();
+        bOutdatedViewMatrix = false;
+    }
 }
 
 void Camera::onKeyPressed(KeyEvent& event)
 {
     switch (event.m_Key)
     {
-        case GLFW_KEY_UP:    bMoveUp = true; break;
-        case GLFW_KEY_DOWN:  bMoveDown = true; break;
-        case GLFW_KEY_LEFT:  bMoveLeft = true; break;
+        case GLFW_KEY_SPACE : bMoveUp = true; break;
+
+        case GLFW_KEY_UP:    bMoveFront    = true; break;
+        case GLFW_KEY_W:     bMoveFront    = true; break;
+
+        case GLFW_KEY_DOWN:  bMoveBack  = true; break;
+        case GLFW_KEY_S:     bMoveBack  = true; break;
+
+        case GLFW_KEY_LEFT:  bMoveLeft  = true; break;
+        case GLFW_KEY_A:     bMoveLeft  = true; break;
+
         case GLFW_KEY_RIGHT: bMoveRight = true; break;
+        case GLFW_KEY_D:     bMoveRight = true; break;
     }
 }
 
 void Camera::onKeyReleased(KeyEvent& event)
 {
-
     switch (event.m_Key)
     {
-        case GLFW_KEY_UP:   bMoveUp = false; break;
-        case GLFW_KEY_DOWN: bMoveDown = false; break;
+        case GLFW_KEY_SPACE: bMoveUp = false; break;
+
+        case GLFW_KEY_UP:    bMoveFront = false; break;
+        case GLFW_KEY_W:     bMoveFront = false; break;
+
+        case GLFW_KEY_DOWN:  bMoveBack = false; break;
+        case GLFW_KEY_S:     bMoveBack = false; break;
+
         case GLFW_KEY_LEFT:  bMoveLeft = false; break;
+        case GLFW_KEY_A:     bMoveLeft = false; break;
+
         case GLFW_KEY_RIGHT: bMoveRight = false; break;
+        case GLFW_KEY_D:     bMoveRight = false; break;
     }
 }
 
 void Camera::onMouseClickEvent(MouseClick& event)
 {
-    if(event.m_Action == GLFW_PRESS && event.m_Mods == GLFW_MOD_ALT)
+    
+    if(event.m_Button == GLFW_MOUSE_BUTTON_LEFT && event.m_Action == GLFW_PRESS )
     {
-        bLookAround = true;
+        firstClick = true;
+        lookAroundState = true;
+        m_WindowHandle->setCursorVisibility(false);
     }
-    else if(event.m_Action == GLFW_RELEASE && bLookAround)
+    else if(event.m_Button == GLFW_MOUSE_BUTTON_LEFT && event.m_Action == GLFW_RELEASE)
     {
-        bLookAround = false;
+        lookAroundState = false;
+        m_WindowHandle->setCursorVisibility(true);
     }
 }
 
 void Camera::onMouseMove(MouseMove& event)
 {
-    if(bLookAround)
+    glm::vec2 current(event.getX(), event.getY());
+
+    Mouse::Delta = current - Mouse::LastPos;
+
+    if(lookAroundState && Mouse::Delta != glm::vec2(0.0f))
     {
-        //
+        yaw += Mouse::Delta.x * m_LookSpeed;
+        pitch += -Mouse::Delta.y * m_LookSpeed;
+
+        pitch = glm::clamp<float>(pitch, -89.0, 89.0);
+
+        rotate(m_deltaTime);
     }
-}
+
+    Mouse::LastPos = current;
+} 
 
 void Camera::onMouseScroll(MouseScroll& event)
 {
+    move(front * (float)event.getYOffset() * m_MoveSpeed * 10.0f, m_deltaTime);
+}
 
+void Camera::updateViewMatrix()
+{
+    view = glm::lookAt(position, position + front, up);
 }
 
 
 void Camera::move(const glm::vec3& dir, float deltaTime)
 {
+    // Update 
+    right = glm::normalize(glm::cross(front, up));
+
     // Update camera's position
-    position += dir * m_BaseSpeed * deltaTime;
+    position += dir * m_MoveSpeed * deltaTime;
 
     // Update view matrix
-    view = glm::lookAt(position, position + target, up);
+    view = glm::lookAt(position, position + front, up);
+}
+    
+void Camera::rotate(float deltaTime)
+{
+    front.x = glm::cos(glm::radians(yaw)) * glm::cos(glm::radians(pitch));
+    front.y = glm::sin(glm::radians(pitch));
+    front.z = glm::sin(glm::radians(yaw)) * glm::cos(glm::radians(pitch));
+
+    front = glm::normalize(front);
+
+    bOutdatedViewMatrix = true; 
 }

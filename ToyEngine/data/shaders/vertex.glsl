fe8549a3bddbbdc2a03ea4003f4f1bc40a6996f3
Embo <79663490+electroma97@users.noreply.github.com>
2021-09-11 18:38:17 +0200

    Add sample glTF models

--- ToyEngine/data/shaders/vertex.glsl
+++ ToyEngine/data/shaders/vertex.glsl
@@ -1,6 +1,7 @@
 #version 330 core
 
 layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aNormal;
 layout(location = 3) in vec3 aColor;
 
 uniform mat4 model;
@@ -11,6 +12,7 @@ out vec4 color;
 
 void main()
 {
-    color = vec4(aColor, 1.0);
+    color = vec4(aNormal, 1.0);
+    
     gl_Position = projection * view * model * vec4(aPos, 1.0f);
 }

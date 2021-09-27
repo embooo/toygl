c7d14ca331c12a1fc74c38f4c2a70e3c9b91b9e6
embooo <79663490+embooo@users.noreply.github.com>
2021-09-27 21:36:49 +0200

    Refactor, add attributes

--- ToyEngine/data/shaders/fragment.glsl
+++ ToyEngine/data/shaders/fragment.glsl
@@ -1,27 +1,56 @@
-#version 330 core
+#version 430 core
 
-in vec4 vPosition;
-in vec3 vNormal;
+out vec4 fragment_color;
 
-uniform vec4 cameraPosition;
+in vec3 pNormal;
+in vec3 p; // position du point de la surface dans le repère camera
+in mat4 viewMat; // passage du repère monde au repère camera
 
-out vec4 FragColor;
+// Lumière ponctuelle
+uniform vec4 cLight; // couleur de la lumière 
+uniform vec3 pLight; // position de la lumière dans le repère monde
+uniform vec3 pCam;  // position de la camera dans le repère monde
 
 const float PI = 3.14;
 
-void main()
+#define LAMBERT
+
+void main( )
 {
-    // Lambert
-    float k = 1.0;
-    float reflectanceFunction = k / PI;
+    // Color
+    vec3 posLight = vec3 ( viewMat * vec4(pLight, 1.0) ) + vec3(0, 0, 0);
+    vec4 baseColor = vec4(0.9, 0.9, 0.9, 1.0);
+    vec4 color = baseColor;
 
-    vec4 baseColor  = vec4(0.8, 0.8, 0.8, 1.0);
-    vec4 lightColor = vec4(0.2, 0.1, 0.23, 1.0);
-    vec3 lightPosition = vec3(0.0, 10.0, 0.0);
+    // surface normal
+    vec3 N = normalize(pNormal);
+    // directions
+    vec3 O = normalize(vec3( (viewMat *  vec4(pCam, 1.0))   - (viewMat * vec4(p, 1.0)) ) ); // observateur 
+    vec3 L = normalize(vec3( (viewMat *  vec4(pLight, 1.0)) - (viewMat * vec4(p, 1.0)) )); // lumière 
 
-    vec3 lightDir =  lightPosition - vec3(vPosition);
+    float cosTheta = dot(N, L) ; // angle
 
-    float cosTheta = dot(normalize(vNormal), normalize(lightDir)) / (length(vNormal) * length(lightDir));
+#ifdef LAMBERT
+    // Modele de lambert
+    float k = 0.9;
+    float fr = k / PI; // fonction de reflection 
 
-    gl_FragColor = baseColor * (reflectanceFunction * cosTheta * lightColor);
+    color *= fr * cosTheta * cLight;
+#endif
+
+
+#ifdef BLINN
+// bissectrice
+    vec3 H = normalize(O + L);
+    
+    float m = 1.0;
+    float cosTheta_H = dot(N, H);
+
+    float fr = ( (m + 8) / (8 * PI)  ) * pow(cosTheta_H, m);
+
+    color *= fr * cosTheta * cLight;
+#endif
+    
+    fragment_color = color;   
 }
+
--- ToyEngine/data/shaders/vertex.glsl
+++ ToyEngine/data/shaders/vertex.glsl
@@ -1,19 +1,25 @@
-#version 330 core
+#version 430 core
 
-layout(location = 0) in vec3 aPos;
-layout(location = 1) in vec3 aNormal;
-layout(location = 3) in vec3 aColor;
+layout(location= 0) in vec3 position;
+layout(location= 1) in vec3 normal;
+layout(location= 2) in vec2 texcoord0;
+layout(location= 3) in vec2 texcoord1;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-out vec3 vNormal;
-out vec4 vPosition;
+out vec3 vNormal; // vertex normal 
+out mat4 viewMat;
+out vec2 vTexcoord;
 
-void main()
+out vec3 p; // position du point de la surface dans le repère monde
+
+void main( )
 {
-    vNormal     = aNormal;
-    vPosition   = projection * view * model * vec4(aPos, 1.0f);
-    gl_Position = vPosition;
+    mat4 mvp = projection * view * model;
+    viewMat = view;
+    vNormal = normal;
+    
+    gl_Position =  mvp * vec4(position, 1.0);
 }

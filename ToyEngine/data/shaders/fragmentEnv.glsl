d42f8182358442b4957fe9dc63085910dcfeeda5
embooo <79663490+embooo@users.noreply.github.com>
2021-10-03 23:13:08 +0200

    Fix lighting :  do lighting calculation in world space

--- ToyEngine/data/shaders/fragmentEnv.glsl
+++ ToyEngine/data/shaders/fragmentEnv.glsl
@@ -14,69 +14,100 @@ struct Sky
     vec3 diffuse;
 };
 
+
+struct PointLight
+{
+    vec3 pos;
+
+    vec3 ambient;
+    vec3 diffuse;
+};
+
 // Uniforms 
+
+// Metallic-Rougness Workflow
 uniform sampler2D baseColorTexture;
 uniform vec4      baseColorFactor;
-uniform int       currentTexcoord;
+uniform int       currentBaseColorTexcoord;
 
 uniform sampler2D normalTexture;
+uniform int currentNormalTexcoord;
+
 
 // Varyings
-in  vec3 vPosition; 
-in  vec3 vNormal; 
+in  vec3 vertexPos; 
+in  vec3 vertexNormal; 
 in  vec2 vTexcoord0; 
 in  vec2 vTexcoord1; 
+in  mat4 matModel; 
 out vec4 fragment_color;
 in vec2  texcoords[2];
 in vec3  cameraPos;
 in mat4 viewMat;
 
 // Constants
-const float PI = 3.14;
+const float PI = 3.141592;
+const vec3  MATE = vec3(0.2, 0.2, 0.2);
 
 #define BLINN
+//#define PER_FRAGMENT_NORMALS
 
 void main( )
 {
-    vec3 normal   = normalize(vNormal);
-    vec3 albedo   = vec3(0.2, 0.2, 0.2);
 
-    DirectionalLight light;
-    light.dir = normalize( vec3(0.6, -0.35, -0.1) );
-    light.ambient = vec3(14.0, 9.0, 5.0);
+    vec3 normalWS     = normalize( vec3( matModel * vec4(vertexNormal, 0.0) ) );
+    vec3 positionWS   = normalize( vec3( matModel * vec4(vertexPos, 0.0) ) );
+
+    #ifdef PER_FRAGMENT_NORMALS
+        vec4 normalTex    = texture(normalTexture, texcoords[0]) ;  
+        normalWS          = vec3( normalize (normalTex * 2.0  - 1.0 ) * matModel ); // value is in [-1,1] range
+    #endif
+
+    vec3 color =  MATE ;
+
+    DirectionalLight dirLight;
+    dirLight.dir     = normalize( vec3(-0.0f, -0.1f, -0.1f) );
+    dirLight.ambient = vec3(7.0, 4.5, 2.5) * 2.0;
+
+    PointLight pointLight;
+    pointLight.pos = vec3(matModel * vec4(0,100,0,1));
+
+    pointLight.ambient = vec3(7.0, 4.5, 2.5);
 
     Sky sky;
-    sky.ambient = vec3(1.0, 1.6, 1.8);
-    sky.diffuse = sky.ambient * dot(0.5 + 0.5 * vec3(0, 1, 0), normal);
+    sky.ambient = vec3(1.4, 0.9, 0.5);
+    sky.diffuse = sky.ambient * dot(normalize(vec3(0.0, -0.6, 0.0)), normalWS) ;
 
-    vec3 color =  albedo ;
+    float cosTheta = max(0, dot(normalWS, normalize(pointLight.pos - positionWS)));
 
-    float cosTheta = max(0, dot(normal, -light.dir));
-
-    light.diffuse  = cosTheta * light.ambient;
+    pointLight.diffuse  = cosTheta * pointLight.ambient;
+    dirLight.diffuse    = cosTheta * dirLight.ambient ;
 
     #ifdef BLINN
-        vec3 O = normalize(cameraPos - vPosition); // observateur 
-        vec3 L = normalize(-light.dir); // lumière 
+
+        vec3 O = normalize(cameraPos - positionWS); // observateur 
+        vec3 L = normalize(pointLight.pos - positionWS); // lumière 
 
         // bissectrice
         vec3 H = normalize(O + L);
     
-        float alpha = 2.0;
-        float cosTheta_H = dot(normal, H);
+        float alpha = 0.1;
+        float cosTheta_H = dot(normalWS, H);
 
         // brdf
-        float fr = ( (alpha + 8) / (8 * PI)  ) * pow(cosTheta_H, alpha);
+        float fr =  ( (alpha + 8) / (8 * PI)  ) * pow(cosTheta_H, alpha);
 
-        color *= fr * light.diffuse;
+        color *= fr * pointLight.diffuse;
     #endif
-        
-    color += albedo * sky.diffuse;
-    
-    vec4 baseColorTex = texture(baseColorTexture, texcoords[currentTexcoord]);
-    vec4 normalTex    = texture(normalTexture, texcoords[currentTexcoord]);
 
+    color += MATE * sky.diffuse;
 
-    fragment_color  = vec4(color, 1.0) * baseColorTex;   
+    vec4 baseColorTex = texture(baseColorTexture, texcoords[currentBaseColorTexcoord]);
+
+    // trans
+    if(baseColorTex.a < 0.3) discard;
+
+    fragment_color  = vec4(color, 1.0) * baseColorTex ;   
+
 }
 
--- ToyEngine/data/shaders/vertex.glsl
+++ ToyEngine/data/shaders/vertex.glsl
@@ -9,13 +9,15 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-out vec3 vPosition;
-out vec3 vNormal; 
+out vec3 vertexPos;
+out vec3 vertexNormal; 
 out mat4 viewMat;
 out vec2 vTexcoord0;
 out vec2 vTexcoord1;
 
 out vec2 texcoords[2];
+
+out mat4 matModel;
 
 out vec3 p; // position du point de la surface dans le repère monde
 
@@ -23,11 +25,14 @@ void main( )
 {
     mat4 mvp = projection * view * model;
     viewMat = view;
-    vNormal = normal;
+
+    vec4 pos =  mvp * vec4(position, 1.0);
     
     texcoords[0] = texcoord0;
     texcoords[1] = texcoord1;
-    vPosition = position;
-
+    vertexPos    = position;
+    vertexNormal = normal;
+    matModel = model;
+    
     gl_Position = mvp * vec4(position, 1.0);
 }

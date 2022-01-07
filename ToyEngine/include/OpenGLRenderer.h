0c566c6f4ff9666ba738f867e77ea731274b6dd8
embooo <79663490+embooo@users.noreply.github.com>
2022-01-07 22:37:04 +0100

    Refactor, add statistics struct

--- ToyEngine/include/OpenGLRenderer.h
+++ ToyEngine/include/OpenGLRenderer.h
@@ -3,6 +3,11 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>  
 
+#include "shader.h"
+#include "gltfModel.h"
+#include "Camera.h"
+#include "Light.h"
+
 #include <Window.h>
 
 class OpenGLRenderer
@@ -15,6 +20,26 @@ public:
     void setClearColor(float r, float g, float b, float a = 1.0f);
     void clear();
 
+    // gltf specific
+    void render(gltf::Model& model, Shader& program, Camera& camera, Light& light);
+
+    struct RenderStats
+    {
+        uint32_t numDrawCalls = 0;
+        uint32_t numIndices   = 0;
+        uint32_t numVertices  = 0;
+
+        void reset() { memset(this, 0, sizeof(*this)); }
+    };
+
+    RenderStats statistics;
+
+    // todo : remove 
+    glm::vec3 translation, scale;
 private:
     glm::vec4 clearColor;
+
+    // gltf specific
+    void drawNode(gltf::Node* node, Shader& program, gltf::Model& model);
+
 };
--- ToyEngine/src/OpenGLRenderer.cpp
+++ ToyEngine/src/OpenGLRenderer.cpp
@@ -20,6 +20,95 @@ void OpenGLRenderer::init()
     //glCullFace(GL_BACK); 
 }
 
+void OpenGLRenderer::render(gltf::Model& model, Shader& program, Camera& camera, Light& light)
+{
+    program.use();
+
+    model.m_VBO.bind();
+    model.m_VAO.bind();
+
+    statistics.reset();
+    
+    //program.setMat4("model", );
+    program.setMat4("view",  camera.getViewMat());
+    program.setMat4("projection", camera.getProjectionMat());
+
+    program.setFloat3("cameraPos",  camera.pos());
+    program.setFloat3("lightPos",   light.pos());
+    program.setFloat4("lightColor", light.color());
+
+    program.setFloat("lightRadius", ((PointLight&)light).radius());
+
+
+    for (gltf::Node* node : model.nodes)
+    {
+        if (node->mesh)
+        {
+            drawNode(node, program, model);
+        }
+
+        if (!node->children.empty())
+        {
+            for (gltf::Node* node : node->children)
+            {
+                if (node->mesh)
+                {
+                    drawNode(node, program, model);
+                }
+            }
+        }
+    }
+}
+
+void OpenGLRenderer::drawNode(gltf::Node* node, Shader& program, gltf::Model& model)
+{
+    program.setMat4("model", node->getGlobalTransform() * glm::scale(scale) * glm::translate(translation));
+
+    for (const gltf::Primitive* p : node->mesh->primitives)
+    {
+        const gltf::Material& material = model.materials.at(p->materialID);
+        if (material.hasBaseColorTexture)
+        {
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, material.baseColorTexture->glBufferId);
+            program.setInt("baseColorTexture", 0); // set texture unit's ID, not the texture buffer's id !
+            program.setInt("currentBaseColorTexcoord", material.baseColorTexture->texCoordSet);
+        }
+
+        if (material.hasNormalTexture)
+        {
+            glActiveTexture(GL_TEXTURE1);
+            glBindTexture(GL_TEXTURE_2D, material.normalTexture->glBufferId);
+            program.setInt("normalTexture", 1);
+            program.setInt("currentNormalTexcoord", material.normalTexture->texCoordSet);
+        }
+
+        if (material.hasMetallicRoughnessTexture)
+        {
+            glActiveTexture(GL_TEXTURE2);
+            glBindTexture(GL_TEXTURE_2D, material.metallicRoughnessTexture->glBufferId);
+            program.setInt("metallicRoughnessTexture", 2);
+            program.setFloat("metallicFactor", material.metallicFactor);
+            program.setFloat("roughnessFactor", material.roughnessFactor);
+            program.setInt("currentMetallicRoughnessTexcoord", material.metallicRoughnessTexture->texCoordSet);
+        }
+
+        if (material.hasEmissiveTexture)
+        {
+            glActiveTexture(GL_TEXTURE3);
+            glBindTexture(GL_TEXTURE_2D, material.emissiveTexture->glBufferId);
+            program.setInt("emissiveTexture", 3);
+            program.setFloat3("emissiveFactor", material.emissiveFactor);
+        }
+
+        glDrawElements(p->mode, p->numIndices, GL_UNSIGNED_INT, (void*)(p->startIndices * sizeof(GL_UNSIGNED_INT)));
+        
+        statistics.numDrawCalls += 1;
+        statistics.numIndices   += p->numIndices;
+        statistics.numVertices  += p->numVertices;
+    }
+}
+
 void OpenGLRenderer::setViewport(int width, int height)
 {
     glViewport(0, 0, width, height);
